81c6db491ccf4f229bbec6757de2350a168e291d
kevinsmith <kevinsmith77@verizon.net>
2025-10-08 20:02:02 -0400

    Added file input

--- main.c
+++ main.c
@@ -7,9 +7,18 @@ FUNC:
 #include <stdio.h>
 
 
-int main() {
-    printf("TEST");
-    printf("Hello, World!\n");
-    
+int main(int argc, char **argv) {
+    float number;
+    FILE *fp;
+
+    fp = fopen (argv[1], "r");
+    if (fp) {
+        while (fscanf (fp, "%f", &number) != EOF) {
+            printf ("%8.2f\n", number);
+        }
+        fclose (fp);
+    } else
+        printf ("ERROR - File not found\n");
+
     return 0;
 }
