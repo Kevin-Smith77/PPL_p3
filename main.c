7499f03f964c1257212d3e1b7cb070fbeb9e92ce
kevinsmith <kevinsmith77@verizon.net>
2025-10-15 19:09:06 -0400

    added arrays and printing

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ FUNC:
 */
 
 #include <stdio.h>
+#include <string.h>
 
 
 /* Global initialization of token values */
@@ -17,17 +18,35 @@ struct error{
 void operator();
 void digit();
 void character();
+void printTokens(char**,int);
 
 int main(int argc, char **argv) {
     FILE *fp;
+    char** tokenArray = (char**) malloc(100 * sizeof(char*));
+    for(int i = 0; i < 100; i++){
+        tokenArray[i] = (char*) malloc(10 * sizeof(char));
+    }
+    int numTokens = 0;
     fp = fopen (argv[1], "r");
     if (fp) {
         while ((token = fgetc(fp)) != EOF) {
-            printf ("%c\n", token);
+            while(token != '\n'){
+
+            }
+            printf ("\n");
+            printTokens(tokenArray, numTokens);
+            numTokens = 0;
         }
         fclose (fp);
     } else
         printf ("ERROR - File not found\n");
 
     return 0;
+}
+
+void printTokens(char** tokenArray, int numTokens){
+    for(int i = 0; i < numTokens; i++){
+        printf("%s  ", tokenArray[i]);
+    }
+    printf("\n");
 }
