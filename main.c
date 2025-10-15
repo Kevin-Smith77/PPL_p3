63de4a14f61ef17d34ac608b3c9be3bc7595854b
Benmmcintire <166653449+Benmmcintire@users.noreply.github.com>
2025-10-15 19:09:00 -0400

    Update main.c

--- main.c
+++ main.c
@@ -5,29 +5,40 @@ FUNC:
 */
 
 #include <stdio.h>
-
+#include <stdbool.h>
 
 /* Global initialization of token values */
-char token, nextToken;
-/* Error struct prototype for when we need it */
-struct error{
-    int line, space, errorCode;
-};
+char c;
 /* Default Constructors */
 void operator();
 void digit();
 void character();
+bool isLetter(char);
 
 int main(int argc, char **argv) {
     FILE *fp;
     fp = fopen (argv[1], "r");
     if (fp) {
-        while ((token = fgetc(fp)) != EOF) {
-            printf ("%c\n", token);
+        while ((c = fgetc(fp)) != EOF) {
+            if(isDigit(c)){}
+            else if(isLetter(c)){}
+            else{}
         }
         fclose (fp);
     } else
         printf ("ERROR - File not found\n");
 
     return 0;
+}
+
+
+/* 
+FUNC:
+INPUT:
+OUTPUT:
+*/
+bool isLetter(char c){
+    int val = (int)c;
+    if(( val >= 65 && val <= 90 )||( val >= 97 && val <= 122)){ return 1; }
+    else{ return 0;}
 }
