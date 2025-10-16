e40b09638edca8800a33b97af6f4d02c89d747f1
Benmmcintire <166653449+Benmmcintire@users.noreply.github.com>
2025-10-15 20:22:34 -0400

    Update main.c
    void operator() added

--- main.c
+++ main.c
@@ -10,9 +10,9 @@ FUNC:
 /* Global initialization of token values */
 char c;
 /* Default Constructors */
-void operator();
-void digit();
-void character();
+void operator(FILE*, char**, int*);
+void digit(FILE*, char**, int*);
+void character(FILE*, char**, int*);
 bool isLetter(char);
 
 void printTokens(char**,int);
@@ -26,8 +26,8 @@ int main(int argc, char **argv) {
     int numTokens = 0;
     fp = fopen (argv[1], "r");
     if (fp) {
-        while ((token = fgetc(fp)) != EOF) {
-            while(token != '\n'){
+        while ((c = fgetc(fp)) != EOF) {
+            while(c != '\n'){
               if(isDigit(c)){}
               else if(isLetter(c)){}
               else{}
@@ -53,10 +53,69 @@ bool isLetter(char c){
     int val = (int)c;
     if(( val >= 65 && val <= 90 )||( val >= 97 && val <= 122)){ return 1; }
     else{ return 0;}
+}
 
+
+/* 
+FUNC:
+INPUT:
+OUTPUT:
+*/
 void printTokens(char** tokenArray, int numTokens){
     for(int i = 0; i < numTokens; i++){
         printf("%s  ", tokenArray[i]);
     }
     printf("\n");
 }
+
+
+
+
+/* 
+FUNC:
+INPUT:
+OUTPUT:
+*/
+void operator(FILE *fp, char** tokenArray, int i){
+    printf(%c, c);
+    if (c == '+' || c == '-'){ 
+        tokenArray[*i++] = "<ADD-OP>"; 
+        c = fgetc(fp);
+    }
+
+    else if (c == '=' ){
+        tokenArray[*i++] = "<EQ-OP>";
+        c = fgetc(fp);
+    }
+
+    else if (c == '!'){
+        c = fgetc(fp);
+        printf("%c", c);
+        if((c == '='){ tokenArray[*i++] = "<EQ-OP>"; }
+        else{ tokenArray[*i++] = "<OTHER>"; }
+        c = fgetc(fp);
+    }
+    else if (c == '>' || c == '<'{
+        tokenArray[*i++] = "<COMP-OP>";
+        c = fgetc(fp);
+        if(c == '='){
+            printf("%c", c);
+            c = fgetc(fp);
+        }    
+    }
+    else if (c == '*' || c == '/' || c == '%'){ 
+        tokenArray[*i++] = "<MULT-OP>"; 
+        c = fgetc(fp);
+    }
+    else if (c == ';'){
+        tokenArray[*i++] = "<NULL-STMT>"; 
+        c = fgetc(fp);
+    }
+    else{ 
+        tokenArray[*i++] = "<OTHER>"; 
+        c = fgetc(fp);
+    }
+}
+
+
+
